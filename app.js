592d13fed8303638261c9d10b70ae7e18c87f8c5
gonzalolsk <gonzalolsk@gmail.com>
2021-05-27 20:20:29 -0300

    modificado app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.listen(3030, ()=> console.log('ya esta corriendo el servidor'));
 
+app.set("port" , process.env.PORT|| 3000)
+
+// app.listen(3030, ()=> console.log('ya esta corriendo el servidor'));
+
+app.listen(app.get("port"),()=>console.log("Server Start http://localhost"+app.get("port")))
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 app.use(express.static(path.resolve(__dirname,'public')))
