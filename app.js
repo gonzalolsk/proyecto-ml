54a47efd345f3ccaac53ea15843fe0501d6b21ec
gonzalolsk <gonzalolsk@gmail.com>
2021-05-27 20:22:45 -0300

    modif app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("port" , process.env.PORT|| 3000)
 
 // app.listen(3030, ()=> console.log('ya esta corriendo el servidor'));
 
-app.listen(app.get("port"),()=>console.log("Server Start http://localhost"+app.get("port")))
+app.listen(app.get("port"),()=>console.log("Server Start http://localhost"+app.get("port")));
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 app.use(express.static(path.resolve(__dirname,'public')))
